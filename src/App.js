99313d961d758623bac6dd47f1112a3ccd18c61e
Andrés <andrexmarrugo@gmail.com>
2019-03-04 12:56:39 -0500

    projecto base para front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Gallery from "./gallery";
+
+class App extends Component {
+  render() {
+    return (
+      <Gallery/>
+    );
+  }
+}
+
+export default App;
---
+++ src/gallery/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavSearchBar from "./NavSearchBar";
+
+export default class Gallery extends Component {
+  render() {
+    return (
+      <div>
+          <NavSearchBar/>
+          <h2>hola</h2>
+        
+      </div>
+    )
+  }
+}
